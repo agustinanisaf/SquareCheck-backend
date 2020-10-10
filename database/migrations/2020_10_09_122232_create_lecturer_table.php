9159e5a9149aafa808da4e3d741a02a31890ec61
fadlikamohamad <fadlikamohamad@gmail.com>
2020-10-10 13:26:05 +0700

    Create User, Lecturer, Student, Department Migration Fixes #1

---
+++ database/migrations/2020_10_09_122232_create_lecturer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecturer', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('nip');
+            $table->integer('department_id');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecturer');
+    }
+}
