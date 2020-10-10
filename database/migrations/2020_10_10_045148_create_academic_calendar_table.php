bb3eb619740e0838c905286aca6cdb1a65279ee3
agustinanisaf <agusteeenisaf@gmail.com>
2020-10-11 00:04:17 +0700

    Create Kalender Akademik Migration Fixes #5

---
+++ database/migrations/2020_10_10_045148_create_academic_calendar_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAcademicCalendarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academic_calendar', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedBigInteger('type');
+            $table->foreign('type')
+                  ->references('id')
+                  ->on('academic_calendar_type')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academic_calendar');
+    }
+}
