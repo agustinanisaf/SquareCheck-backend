70e9fa4b1960440e7e457465cd6f630e7ccccd93
rizabach <riza.bachtiar.rb@gmail.com>
2020-10-14 21:52:45 +0700

    Sudah bikin migration + model untuk schedule, subject dan student attendance

--- app/Models/StudentAttendance.php
+++ app/Models/StudentAttendance.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class StudentAttendance extends Model
 {
-    /**
-     * Get the phone record associated with the user.
-     */
+		
 }
--- database/migrations/2020_10_14_133448_create_subjects_table.php
+++ database/migrations/2020_10_14_133448_create_subjects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class StudentAttendance extends Migration
+class CreateSubjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class StudentAttendance extends Migration
      */
     public function up()
     {
-			Schema::create('student_attendance', function(Blueprint $table) {
-				$table->integer('schedule_id')->references('id')->on('schedule');
-				$table->integer('student_id')->references('id')->on('student');
-				$table->date('time');
-				$table->enum('status',['alpa, hadir, izin, telat']);
+			Schema::create('subjects', function(Blueprint $table) {
+				$table->id();
+				$table->char('name', 100);
+				$table->integer('id_lecturer');
 			});
     }
 
@@ -28,6 +27,6 @@ class StudentAttendance extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('subjects');
     }
 }
