5e7f265e1e8aef0ca955318c37661edde29149d0
AanMFA <aanmfa06@gmail.com>
2020-10-15 20:47:32 +0700

    Create All Database Seeder Fixes #19

---
+++ database/seeders/TokenSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TokenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tokens')->insert([
+            'user_id' => User::all()->pluck('id'),
+            'token' => Str::random(10),
+            'type' => null
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'username' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => rand(1, time()),
+            'is_admin' => rand(false, true),
+            'photo' => Str::random(10).'.png'
+        ]);
+    }
+}
