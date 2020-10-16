9aaf2dac35941e20dd5fc5d0f4e37f6a17a35e8e
Riza Nafis <riza.nafis@gmail.com>
2020-10-16 09:05:01 +0700

    Add Seeder and Factory Class Fixes Create All Database Seeder #19

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -23,7 +25,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
+            'username' => Str::random(10),
             'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password'),
+            'email_verified_at' => $this->faker->randomElement([null, $this->faker->dateTime($max = 'now', $timezone = null)]),
+            'is_admin' => $this->faker->randomElement([false, true]),
+            'photo' => Str::random(10).'.png'  
         ];
     }
 }
