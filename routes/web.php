1f772a575ea27920260ce6995800322e99ad6da0
Riza Nafis <riza.nafis@gmail.com>
2020-10-17 14:00:05 +0700

    Add UserController + Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->group(['prefix' => 'users'], function () use ($router) {
+        $router->get('', [
+            'as' => 'user.index', 'uses' => 'UserController@index'
+        ]);
+
+        $router->get('{id}', [
+            'as' => 'user.show', 'uses' => 'UserController@show'
+        ]);
+
+        $router->post('', [
+            'as' => 'user.store', 'uses' => 'UserController@store'
+        ]);
+
+        $router->put('{id}', [
+            'as' => 'user.update', 'uses' => 'UserController@update'
+        ]);
+
+        $router->delete('{id}', [
+            'as' => 'user.destroy', 'uses' => 'UserController@destroy'
+        ]);
+
+        $router->get('{id}/tokens', [
+            'as' => 'user.token', 'uses' => 'UserController@getToken'
+        ]);
+
+        $router->post('{id}/tokens', [
+            'as' => 'user.token', 'uses' => 'UserController@createToken'
+        ]);
+
+        // TODO: Add Student and Lecturer (and Admin, based on new Database design)
+    });
+});
